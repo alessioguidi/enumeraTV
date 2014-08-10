7d67092d6dca438cedb76e15119c763133d9aa61
alessioguidi <alessio.guidi@gmail.com>
2014-08-11 00:05:02 +0200

    release2

--- pages/Sincro/Sincro.js
+++ pages/Sincro/Sincro.js
@@ -1,9 +1,91 @@
 dojo.declare("Sincro", wm.Page, {
 	start: function() {
-		
+		document.addEventListener("deviceready", this.handleDeviceReady, false);
 	},
 	"preferredDevice": "desktop",
+    
+    handleDeviceReady: function (event) {   
+        // onSuccess Callback
+        // This method accepts a Position object, which contains the
+        // current GPS coordinates
+        //
+       // navigator.splashscreen.hide();
+        document.addEventListener("backbutton", onBackButton, false);
+        /*
+        document.addEventListener("searchbutton", onSearch, false);
+        document.addEventListener("menubutton", onMenuButton, false);
+        
+        document.addEventListener("online", onOnline, false);
+        document.addEventListener("offline", onOffline, false);
 
+        function onOnline() {
+            this.pictureNetwork.setImageIndex(49);
+        }
+        function onOffline() {
+            this.pictureNetwork.setImageIndex(50);
+        }        
+        // Handle the backbutton
+        //
+        function onBackButton() {
+            alert("You hit the back button!");
+        }
+        // Handle the menubutton
+        //
+        function onMenuButton() {
+            alert("You hit the menu button!");
+        }
+        // Handle the searchbutton
+        //
+        function onSearchButton() {
+            alert("You hit the search button!");
+        }
+        */
+        var onSuccess = function(position) {
+            
+        /*    
+            app.deviceInformation.setData([{
+                "uuid":device.uuid,
+                "name":device.name,
+                "model":device.model,
+                "platform":device.platform,
+                "version":device.version,
+                "phonenumber":"-",
+                "latitude": position.coords.latitude,
+                "longitude": position.coords.longitude,
+                "altitude": position.coords.altitude,
+                "speed": position.coords.speed,
+                "gpstimestamp": position.timestamp,
+                "dataValue": 0}]);
+                
+                //window.plugin.backgroundMode.disable();
+                app.serviceNotify.update();
+                */
+                alert("ci sono");
+                
+                
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }
+        
+        function onBackButton() {
+            //navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
+            app.confirmExit.update();
+        }
+        
+
+     
+        navigator.geolocation.watchPosition(onSuccess, onError,{
+            maximumAge: app.parGpsMaxAge.getValue("dataValue"),
+            timeout: app.parGpsTimeout.getValue("dataValue"),
+            enableHighAccuracy: false});
+        
+
+    },
 	button1Click: function(inSender) {
 		// !! Assumes variable fileURL contains a valid URL to a path on the device,
 //    for example, cdvfile://localhost/persistent/path/to/downloads/
