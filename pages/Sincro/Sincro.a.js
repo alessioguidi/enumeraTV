f5bb12b472b694f48e1923a4527e0233891b00af
alessioguidi <alessio.guidi@gmail.com>
2014-08-13 19:43:30 +0200

    rel 0.304

--- pages/Sincro/Sincro.a.js
+++ pages/Sincro/Sincro.a.js
@@ -4,97 +4,29 @@ document.addEventListener("deviceready", this.handleDeviceReady, false);
 },
 "preferredDevice": "desktop",
 handleDeviceReady: function (event) {
-// onSuccess Callback
-// This method accepts a Position object, which contains the
-// current GPS coordinates
-//
-// navigator.splashscreen.hide();
-window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFSSuccess, onError);
-/*
-document.addEventListener("searchbutton", onSearch, false);
-document.addEventListener("menubutton", onMenuButton, false);
-document.addEventListener("online", onOnline, false);
-document.addEventListener("offline", onOffline, false);
-function onOnline() {
-this.pictureNetwork.setImageIndex(49);
-}
-function onOffline() {
-this.pictureNetwork.setImageIndex(50);
-}
-// Handle the backbutton
-//
-function onBackButton() {
-alert("You hit the back button!");
-}
-// Handle the menubutton
-//
-function onMenuButton() {
-alert("You hit the menu button!");
-}
-// Handle the searchbutton
-//
-function onSearchButton() {
-alert("You hit the search button!");
-}
-*/
-var onSuccess = function(position) {
-/*
-app.deviceInformation.setData([{
-"uuid":device.uuid,
-"name":device.name,
-"model":device.model,
-"platform":device.platform,
-"version":device.version,
-"phonenumber":"-",
-"latitude": position.coords.latitude,
-"longitude": position.coords.longitude,
-"altitude": position.coords.altitude,
-"speed": position.coords.speed,
-"gpstimestamp": position.timestamp,
-"dataValue": 0}]);
-//window.plugin.backgroundMode.disable();
-app.serviceNotify.update();
-*/
-alert("ci sono");
-};
-function gotFS(fs) {
-var fail = failCB('getFile');
-var fileURL = "cdvfile://localhost/persistent/enumera/clienti.txt";
-fs.root.getFile(fileURL, {create: true, exclusive: false},
-gotFileEntry, fail);
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+function gotFS(fileSystem) {
+fileSystem.root.getFile("clienti.txt", null, gotFileEntry, fail);
 }
 function gotFileEntry(fileEntry) {
-var fail = failCB('createWriter');
-file.entry = fileEntry;
 fileEntry.createWriter(gotFileWriter, fail);
-readText();
 }
-function gotFileWriter(fileWriter) {
-file.writer.available = true;
-file.writer.object = fileWriter;
+function gotFileWriter(writer) {
+writer.onwrite = function(evt) {
+alert("write success");
+};
+writer.write("some sample text");
+// contents of file now 'some sample text'
+writer.truncate(11);
+// contents of file now 'some sample'
+writer.seek(4);
+// contents of file still 'some sample' but file pointer is after the 'e' in 'some'
+writer.write(" different text");
+// contents of file now 'some different text'
 }
-function saveText(e) {
-if (file.writer.available) {
-file.writer.available = false;
-file.writer.object.onwriteend = function (evt) {
-file.writer.available = true;
-file.writer.object.seek(0);
+function fail(error) {
+alert(error.code);
 }
-file.writer.object.write("cicciopoccio");
-}
-return false;
-}
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-alert('code: '    + error.code    + '\n' +
-'message: ' + error.message + '\n');
-}
-function onBackButton() {
-//navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
-app.confirmExit.update();
-}
-saveText();
 },
 button1Click: function(inSender) {
 // !! Assumes variable fileURL contains a valid URL to a path on the device,
